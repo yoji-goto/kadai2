e0ea40917a0a689498cb9236805c95ee6a9fb869
YojiGoto <117175808+yoji-goto@users.noreply.github.com>
2022-11-07 15:05:04 +0000

    [add]投稿機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,24 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/show'
+  end
+
   def index
+    @book = Book.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
