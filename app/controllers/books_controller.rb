13d7cd230bd1617e7d1c532885fb81723be77323
YojiGoto <117175808+yoji-goto@users.noreply.github.com>
2022-11-11 14:43:58 +0000

    [Add]editエラーメッセージの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,18 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
     flash[:notice] = "Book was successfully created."
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
+    else
+    render :index
+    end
   end
 
   def index
-    @new = Book.new
+    @book = Book.new
     @books = Book.all
   end
 
@@ -23,10 +27,14 @@ class BooksController < ApplicationController
   end
 
   def update
+     @book = Book.find(params[:id])
     book = Book.find(params[:id])
-    book.update(book_params)
+    if @book.update(book_params)
     flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
+    else
+    render :edit
+    end
   end
 
   def destroy
