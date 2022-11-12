23e8899e1e060db4963f3c3979109fa50b66686b
YojiGoto <117175808+yoji-goto@users.noreply.github.com>
2022-11-12 16:06:42 +0000

    [Finish]1

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get '/top' => 'homes#top'
-  get '/books' => 'books#index', as: 'index_book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  get 'books/:id' => 'books#show',as: 'book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'book/:id' => 'books#destroy', as: 'destroy_book'
+  root to: 'homes#top'
   resources :books
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
